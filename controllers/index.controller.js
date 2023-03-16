a55a4c1f0752c1b14918206102eca97705244d25
LongND <longnd@sphinxjsc.com>
2023-03-17 02:57:26 +0700

    cm code

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,35 +1,26 @@
 sap.ui.define(
-  [
-    "sap-app/controllers/BaseController",
-    "sap/ui/model/json/JSONModel",
-  ],
-  function (BaseController,JSONModel) {
+  ["sap-app/controllers/BaseController", "sap/ui/model/json/JSONModel"],
+  function (BaseController, JSONModel) {
     "use strict";
     return BaseController.extend("sap-app.controllers.index", {
-      stringCall: "Hello Long",
       onShowHello: function () {
-        // show a native JavaScript alert
-        // alert(this.stringCall);
-        // console.log(this, "this")
         this.getRouter().navTo("listItems");
       },
-      onChangeInput: function () {
-        // console.log(this.getView().getModel().oData.user.name);
-      },
       onInit: function () {
-        fetch("../models/data.json")
-          .then((res) => res.json())
-          .then((res) => {
-            const model = new JSONModel({
-              users: res.data,
-              user: {
-                name: "Long",
-                age: "123",
-              },
-            });
-            this.getView().setModel(model);
-            console.log(res, "res");
-          });
+        console.log(345)
+        // fetch("../models/data.json")
+        //   .then((res) => res.json())
+        //   .then((res) => {
+        //     const model = new JSONModel({
+        //       users: res.data,
+        //       user: {
+        //         name: "Long",
+        //         age: "123",
+        //       },
+        //     });
+        //     this.getView().setModel(model);
+        //     console.log(res, "res");
+        //   });
         // fetch("../models/data.json")
         //   .then((res) => res.json())
         //   .then((res) => {
@@ -43,8 +34,6 @@ sap.ui.define(
         //     this.getView().setModel(model);
         //     console.log(res);
         //   });
-        
-        
       },
     });
   }
--- controllers/list-items.controller.js
+++ controllers/list-items.controller.js
@@ -3,18 +3,21 @@ sap.ui.define(
   function (BaseController, JSONModel) {
     "use strict";
     return BaseController.extend("sap-app.controllers.list-items", {
-    //   onInit: function () {
-    //     fetch("../models/data.json")
-    //       .then((res) => res.json())
-    //       .then((res) => {
-    //         const model = new JSONModel({ users: res.data, user: {
-    //             name: "Long",
-    //             age: "123"
-    //           }});
-    //         this.getView().setModel(model);
-    //       });
-        
-    //   },
+      onInit: function () {
+        console.log(123);
+        fetch("../models/data.json")
+          .then((res) => res.json())
+          .then((res) => {
+            const model = new JSONModel({
+              users: res.data,
+              user: {
+                name: "Long",
+                age: "123",
+              },
+            });
+            this.getView().setModel(model);
+          });
+      },
     });
   }
 );
