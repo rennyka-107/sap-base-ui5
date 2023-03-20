5aad33158b429ca7b6b25b91435089e6aa972698
LongNd <longnd@sphinxjsc.com>
2023-03-20 11:10:40 +0700

    cm code

--- controllers/Employee/Detail.controller.js
+++ controllers/Employee/Detail.controller.js
@@ -1,13 +1,22 @@
 sap.ui.define(
   ["sap-app/controllers/BaseController", "sap/ui/model/json/JSONModel"],
   function (BaseController, JSONModel) {
-    "use strict";
     return BaseController.extend("sap-app.controllers.Employee.Detail", {
-      onInit: function () {
-        
+      // onInit: function () {
+      //   var oRouter = this.getRouter();
+      //   console.log(oRouter, "o router")
+      // },
+      onAfterRendering: function() {
+        const {id} = this.getCurrentParams();
+        if(id) {
+          let model = new JSONModel({id});
+          this.getView().setModel(model);
+        }
       },
-      onAfterRendering: function () {
-        console.log("after rendering");
+      getCurrentParams: function(router = this.getOwnerComponent().getRouter()) {
+        const currentHash = router.getHashChanger().getHash();
+        const { arguments } = router.getRouteInfoByHash(currentHash);
+        return arguments;
       },
     });
   }
